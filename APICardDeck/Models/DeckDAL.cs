b4a13766d1b0a2aab5c77f4e68c93a9556d82f44
allison <allisonrjones@gmail.com>
2023-02-13 15:40:26 -0500

    final

---
+++ APICardDeck/Models/DeckDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace APICardDeck.Models
+{
+    public class DeckDAL
+    {
+        public static DeckModel GetDeck()  //THIS IS A PLUG AND PLAY METHOD
+        {
+            //setup
+
+            string url = "https://deckofcardsapi.com/api/deck/new/draw/?count=5";
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Convert to C#
+            DeckModel result = JsonConvert.DeserializeObject<DeckModel>(JSON);
+            return result;
+        }
+    }
+}
+
+
+
+
+
